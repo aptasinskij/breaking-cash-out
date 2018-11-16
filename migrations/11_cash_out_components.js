f877a8cdc6cbba73cda8f71db4ce0d13a85539ef
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-16 17:13:24 +0200

    fixed migration script

--- migrations/11_cash_out_components.js
+++ migrations/11_cash_out_components.js
@@ -2,7 +2,7 @@ var Config = artifacts.require("Config");
 var CashOutLib = artifacts.require("CashOutLib");
 var CashOutStorage = artifacts.require("CashOutStorage");
 var CashOutManager = artifacts.require("CashOutManager");
-var CashOutOralce = artifacts.require("CashOutOralce");
+var CashOutOracle = artifacts.require("CashOutOracle");
 var CashOutController = artifacts.require("CashOutController");
 
 module.exports = function (deployer) {
@@ -11,5 +11,5 @@ module.exports = function (deployer) {
     deployer.deploy(CashOutController, Config.address);
     deployer.deploy(CashOutStorage, Config.address);
     deployer.deploy(CashOutManager, Config.address);
-    deployer.deploy(CashOutOralce, Config.address);
+    deployer.deploy(CashOutOracle, Config.address);
 };
